c851e984cbde8c0cedb9b1e56d6e5bd45a207828
Pedro <pedro.isidoro26@yahoo.com.br>
2024-03-29 08:18:56 -0300

    Update new codeClass

---
+++ javaScript[40h]/modulo-f-aula-16/index-1.js
@@ -0,0 +1,9 @@
+function oddEven(n){
+    if(n%2 == 0){
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+let result = oddEven(131)
+console.log(result)
---
+++ javaScript[40h]/modulo-f-aula-16/index-2.js
@@ -0,0 +1,5 @@
+function sum(n1=0, n2=0){ //Se não for passado número, o 0 é valor padrão
+    return n1 + n2
+}
+
+console.log(sum(2, 5))
